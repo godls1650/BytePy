aac2ec29eba5988b682b5e407c85cc99f258c67b
godls1650 <55517456+godls1650@users.noreply.github.com>
2020-04-16 21:53:12 +0900

    Create module0325_listBasic.py

---
+++ 재직자/module0325_listBasic.py
@@ -0,0 +1,37 @@
+n = [10,50,60,70,90,80,20,30,40,100]
+# 시작 : 최대 
+y = [2014,2015,2016,2017,2018,2019,2020]
+
+#select = int(input('최근 몇년의 자료가 필요하십니까 > '))
+#print(y[-select : ])
+
+# 리스트의 메서드 
+
+lst = list()
+lst.append(10)# append() 매개 변수를 원소를 추가한다 
+lst.append(20)
+lst.append(30)
+
+temp = [40, 50, '\n'] 
+# sort() 매개변수 없음 , 리스트 내부의 값을 오름차순으로 정렬한다.
+# 내림차순 ?
+"""
+n.append() # 추가
+n.clear() # 내부 원소 삭제
+n.copy() 
+n.count()
+n.extend()
+n.index()
+n.insert()
+n.pop()
+n.remove()
+n.reverse()
+n.sort()
+"""
+
+
+
+
+
+
+
