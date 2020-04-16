05cbcfefd8306e6d47d4ea6047be3c8aa1cce25c
godls1650 <55517456+godls1650@users.noreply.github.com>
2020-04-16 21:53:19 +0900

    Create module0326_iterable2.py

---
+++ 재직자/module0326_iterable2.py
@@ -0,0 +1,24 @@
+# dictionary Type : dict()     a = {}
+# 중복허용안되는 값 : 중복허용값 , 중복허용안되는 값 : 중복허용값 , 중복허용안되는 값 : 중복허용값
+a = {'key1' : 'value2','key2' : 'value2', 'key3' : 'value3', 'key4' : 'value4'}
+# 인덱싱 (X) 키를 인덱스처럼 사용해서 정보 조회
+print(a['key1'])
+a['key1'] = 100
+
+c = {'과일' : ['바나나'], '야채': ['상추', '깻잎', '무']}
+c['과일'].append('사과')
+
+b = 1992 % 100
+
+people = dict()
+people[b] = list()
+people[b].append('이해인')
+
+
+
+
+
+        
+            
+        
+        
