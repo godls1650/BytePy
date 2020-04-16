04343078ceab5b19ded4bbe374f72f811f41f4ae
godls1650 <55517456+godls1650@users.noreply.github.com>
2020-04-16 21:53:08 +0900

    Create module0317_InputFunc.py

---
+++ 재직자/module0317_InputFunc.py
@@ -0,0 +1,14 @@
+#InputFunc.py
+#입력 함수 내부 동작 : windows의 입력버퍼(STDIN)에 저장된 문자열을 가져온다.
+var = int(input('반지름을 입력하세요 > '))
+
+#var는 input결과를 저장하기 때문에 항상 str type
+#var를 형변환
+s = open('C:\\Users\\godls\\Desktop\\Python_TT\\src\\test.txt', 'w')
+
+print('hello world',file = sys.stderr)
+print('hello world')
+
+f.close()
+
+
